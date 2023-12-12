9baec84c601c09f85490c2ffcb29669170bc8999
mazyogz <yogiprass11@gmail.com>
2023-12-12 21:12:55 +0700

    fix params

--- index.js
+++ index.js
@@ -113,7 +113,7 @@ app.put(
   upload.single("image"),
   async (req, res) => {
     try {
-      const eventId = req.params;
+      const eventId = req.params.eventId;
 
       // request body => req.body.name
       const {
