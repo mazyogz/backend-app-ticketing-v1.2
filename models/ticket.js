6cb65860eed9ea1fb9f28e9374b651c5019a68be
mazyogz <yogiprass11@gmail.com>
2023-12-12 20:37:01 +0700

    fix error sequelize

--- models/ticket.js
+++ models/ticket.js
@@ -1,10 +1,9 @@
 'use strict';
-const {
-  Model
-} = require('sequelize');
+const { DataTypes, Model } = require('sequelize');
 const sequelize = require('../config/db.config'); // connect to database railway
 // const sequelize = require('../config/db.local.config'); // connect to database local
-module.exports = (sequelize, DataTypes) => {
+
+module.exports = () => {
   class ticket extends Model {
     /**
      * Helper method for defining associations.
